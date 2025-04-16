5a787648a86329a20381070831add9acc0817064
Khazra Ayub <162344197+Khazraayub@users.noreply.github.com>
2025-04-15 17:29:16 -0700

    assignment-4

---
+++ Assignments 00 to 07/05_loops_control_flow/00_guess_my_number.py
@@ -0,0 +1,35 @@
+# Problem Statement
+# Guess My Number
+
+# I am thinking of a number between 0 and 99... Enter a guess: 50 Your guess is too high
+
+# Enter a new number: 25 Your guess is too low
+
+# Enter a new number: 40 Your guess is too low
+
+# Enter a new number: 45 Your guess is too low
+
+# Enter a new number: 48 Congrats! The number was: 48
+
+
+
+import random
+
+def guess_my_number():
+    # Randomly pick a number between 0 and 99
+    secret_number = random.randint(0, 99)
+    print("I am thinking of a number between 0 and 99...")
+
+    while True:
+        guess = int(input("Enter a guess: "))
+
+        if guess < secret_number:
+            print("Your guess is too low")
+        elif guess > secret_number:
+            print("Your guess is too high")
+        else:
+            print(f"Congrats! The number was: {secret_number}")
+            break
+
+if __name__ == "__main__":
+    guess_my_number()
